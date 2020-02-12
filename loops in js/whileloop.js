95e9cdf5501fde26dff59ef1402577fbc44f9ec9
Piyush Vishnoi <46771423+Piyushvishnoi@users.noreply.github.com>
2020-02-12 23:44:21 +0530

    Update whileloop.js

--- loops in js/whileloop.js
+++ loops in js/whileloop.js
@@ -1,6 +1,6 @@
 num=0;
-while(num<100)
+while(num<100) //loop will run till num have value less than 100
 {
-    num++;
+    num++;  //if this will not be there , then loop will go infinite
     console.log(num);
 }
