9233ffb25c32a3efef18b922d97596791a3e8885
Piyush Vishnoi <46771423+Piyushvishnoi@users.noreply.github.com>
2020-02-12 23:41:30 +0530

    using functions with object in javascript

---
+++ objects in js/objectfunctions.js
@@ -0,0 +1,4 @@
+objectdemo={first:'Piyush',last:'Vishnoi',age:19,height:173,
+studentinfo:function(){ return this.first+this.last +'\n'+ this.age +'\n'+ this.height}
+}
+console.log(objectdemo.studentinfo());
