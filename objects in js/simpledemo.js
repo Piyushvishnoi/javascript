3d5ae1bfa682c7024576ec29eecfcb80cc339337
Piyush Vishnoi <46771423+Piyushvishnoi@users.noreply.github.com>
2020-02-12 23:25:31 +0530

    Update simpledemo.js

--- objects in js/simpledemo.js
+++ objects in js/simpledemo.js
@@ -1,3 +1,3 @@
-objectdemo={first:'Piyush',last:'Vishnoi',age:19,height:173}
+objectdemo={first:'Piyush',last:'Vishnoi',age:19,height:173} // here first is the object of Piyush
 let name=objectdemo.first+objectdemo.last;
 console.log(name); 
