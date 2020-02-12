c7d629a6b51e11e817524387fd29cf8ee28bb771
Piyush Vishnoi <46771423+Piyushvishnoi@users.noreply.github.com>
2020-02-12 23:42:56 +0530

    Update objectfunctions.js

--- objects in js/objectfunctions.js
+++ objects in js/objectfunctions.js
@@ -2,6 +2,6 @@ objectdemo={first:'Piyush',
             last:'Vishnoi',
             age:19,
             height:173,
-            studentinfo:function(){ return this.first+this.last +'\n'+ this.age +'\n'+ this.height}
+            studentinfo:function(){ return this.first+this.last +'\n'+ this.age +'\n'+ this.height} //defining function with the object
 }
 console.log(objectdemo.studentinfo());
