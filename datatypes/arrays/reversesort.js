e7d7bf3c1b736662e629d259b31e87217b4acfc3
Piyush Vishnoi <46771423+Piyushvishnoi@users.noreply.github.com>
2020-02-12 23:13:53 +0530

    reverse, concat,sorting elements of an array

---
+++ datatypes/arrays/reversesort.js
@@ -0,0 +1,8 @@
+let fruits=['banana','apple','keenu','pineapple'];
+let vegetables=['ginger','cauli flower','tomato','potato'];
+revveg=vegetables.reverse();
+console.log(revveg);
+allgroceries=fruits.concat(revveg);
+console.log(allgroceries);
+console.log(allgroceries.sort());
+
