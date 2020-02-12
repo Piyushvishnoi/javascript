e0e4a1f02cda733776d5bd43eb8b7a1bbcab1da9
Piyush Vishnoi <46771423+Piyushvishnoi@users.noreply.github.com>
2020-02-12 13:35:12 +0530

    common string operations explained

---
+++ datatypes/stringdatatypeexplained.js
@@ -0,0 +1,16 @@
+let fruit='bananas';
+console.log(fruit);
+console.log(fruit.indexOf('s'));
+console.log(fruit.length);
+console.log(fruit.slice(3,6));
+console.log(fruit.replace('ban','1234'));
+console.log(fruit.toUpperCase());
+console.log(fruit.toLowerCase());
+for(let i=0;i<fruit.length;i++)
+console.log(fruit.charAt(i));
+//replacement of charAt
+for(let i=0;i<fruit.length;i++)
+console.log(fruit[i]);
+console.log(fruit.split('')); //split by chararcter 
+let fruit1='banana,keenu';
+console.log(fruit1.split(',')); //split by comma 
