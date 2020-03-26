435062a6f6032f9c6227f0f01dc1ab052848f9e2
Piyush Vishnoi <46771423+Piyushvishnoi@users.noreply.github.com>
2020-03-26 23:46:03 +0530

    Update multidimensionalarrays.js

--- datatypes/arrays/multi-dimensional arrays/multidimensionalarrays.js
+++ datatypes/arrays/multi-dimensional arrays/multidimensionalarrays.js
@@ -1,6 +1,8 @@
+//multi-dimensional array
 var myArray = [
   [1, 2, 3],
   ["piyush", "Vishnoi", "khushi"]
 ];
+//accessing a value of multi dimensional array
 var myData = myArray[0][0];
 console.log(myData);
