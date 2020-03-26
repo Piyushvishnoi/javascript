4d02108b25664dafdad93e5201916988e750dbff
Piyush Vishnoi <46771423+Piyushvishnoi@users.noreply.github.com>
2020-03-26 23:38:51 +0530

    Update multiplevaluesinarrays.js

--- datatypes/arrays/array operations/multiplevaluesinarrays.js
+++ datatypes/arrays/array operations/multiplevaluesinarrays.js
@@ -1,5 +1,7 @@
 var ourArray = ["John", 23, 70];
+//assigning only first index
 var myArray = ourArray[0];
 console.log(myArray);
+//changing value of first index 
 ourArray[0] = ["Piyush"];
 console.log(ourArray);
