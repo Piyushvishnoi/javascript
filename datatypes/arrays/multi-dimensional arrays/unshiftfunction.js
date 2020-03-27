27c17f0f4e485d2292e14126380fb1e80aa69341
Piyush Vishnoi <46771423+Piyushvishnoi@users.noreply.github.com>
2020-03-27 23:45:27 +0530

    unshift function on multi-dimensional array

---
+++ datatypes/arrays/multi-dimensional arrays/unshiftfunction.js
@@ -0,0 +1,9 @@
+var myArray = [1, "Piyush", 33];
+myArray.unshift("Happy");
+console.log(myArray);
+var ourArray = [
+  [1, "Piyush"],
+  [2, "Vishnoi"]
+];
+ourArray.unshift([4, "Khushi"]);
+console.log(ourArray);
