02f024e8ad6c033da311163ef732540ef3e431ef
Piyush Vishnoi <46771423+Piyushvishnoi@users.noreply.github.com>
2020-03-27 23:33:07 +0530

    pop operation over multi-dimensional array

---
+++ datatypes/arrays/multi-dimensional arrays/popfunction.js
@@ -0,0 +1,9 @@
+var ourArray = [1, 2, 3];
+var removedFromourArray = ourArray.pop();
+console.log(ourArray);
+var myArray = [
+  ["john", 23],
+  ["cat", 45]
+];
+var removedFrommyArray = myArray.pop();
+console.log(myArray);
