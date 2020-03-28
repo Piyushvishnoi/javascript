cba15b9dce42d2628e6da9f44b7e03499a0f738a
Piyush Vishnoi <46771423+Piyushvishnoi@users.noreply.github.com>
2020-03-28 23:52:25 +0530

    Update diferencebetweenglobalansscope.js

--- scope of variables/diferencebetweenglobalansscope.js
+++ scope of variables/diferencebetweenglobalansscope.js
@@ -1,7 +1,7 @@
 var myvar = "global";
 function fun1() {
   var myvar = "local";
-  return myvar;
+  return myvar;  //here local scope will have more precedence over global scope
 }
 console.log(fun1());
 console.log(myvar);
