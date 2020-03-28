01e92ca3b9652bf1d7be53f6c3da20dc3cc7878f
Piyush Vishnoi <46771423+Piyushvishnoi@users.noreply.github.com>
2020-03-28 18:11:12 +0530

    Update reusablefunction.js

--- functions/reusablefunction.js
+++ functions/reusablefunction.js
@@ -2,8 +2,10 @@ function reusablefunction() {
   console.log("hello world");
 }
 reusablefunction();
+//function with argument
 function ourreusablefunction(a, b) {
   console.log(a + b);
 }
 ourreusablefunction(10, 2);
+//function got reused
 reusablefunction();
