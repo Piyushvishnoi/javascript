a1dcf4859ae9f9fdff5c258ec1009b292f1890ed
Piyush Vishnoi <46771423+Piyushvishnoi@users.noreply.github.com>
2020-03-28 23:06:07 +0530

    Update globalscopefunctions.js

--- scope of variables/globalscopefunctions.js
+++ scope of variables/globalscopefunctions.js
@@ -1,9 +1,9 @@
 var myGlobal = 10;
 function myFunction() {
-  var myVar = 2;
+  var myVar = 2;  //we used "var" and hence its scope is local
 }
 function myFunction1() {
-  myVar1 = 2;
+  myVar1 = 2;  //this will have a by default global scope as "var" is not used 
   //console.log("my variable is" + myVar);
   console.log("my global variable is" + myGlobal);
 }
