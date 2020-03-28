850da12382f11f2ff8de1affd5f5c558f4084a63
Piyush Vishnoi <46771423+Piyushvishnoi@users.noreply.github.com>
2020-03-28 23:21:03 +0530

    Update localscope.js

--- scope of variables/localscope.js
+++ scope of variables/localscope.js
@@ -1,7 +1,7 @@
 var myvar = 23;
 function fun1() {
-  var myvar1 = 34;
+  var myvar1 = 34; //this has a local scope
   console.log(myvar);
 }
-//console.log(myvar1);
+//console.log(myvar1); since myvar1 has local scope hence can't be accesed
 fun1();
