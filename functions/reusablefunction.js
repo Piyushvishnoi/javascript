0277540a7f1f9af7a03f019d2fae7992386f8178
Piyush Vishnoi <46771423+Piyushvishnoi@users.noreply.github.com>
2020-03-28 18:10:29 +0530

    reusable functions with arguments

---
+++ functions/reusablefunction.js
@@ -0,0 +1,9 @@
+function reusablefunction() {
+  console.log("hello world");
+}
+reusablefunction();
+function ourreusablefunction(a, b) {
+  console.log(a + b);
+}
+ourreusablefunction(10, 2);
+reusablefunction();
