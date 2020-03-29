3489711e3b9a84e636e3cde418ea2225f74afd19
Piyush Vishnoi <46771423+Piyushvishnoi@users.noreply.github.com>
2020-03-29 18:35:27 +0530

    using shift and push

--- datatypes/arrays/queuefunctionality.js
+++ datatypes/arrays/queuefunctionality.js
@@ -1,5 +1,7 @@
 function myQueue(arr, item) {
+  //this will push an item in last of array 
   arr.push(item);
+  //this will delete an element at the beginning of array 
   return arr.shift();
 }
 myarr = [1, 2, 3, 4, 5];
