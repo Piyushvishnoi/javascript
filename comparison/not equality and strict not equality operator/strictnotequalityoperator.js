85a5844bccb25a5fafe19df4698dca4e8f2a631c
Piyush Vishnoi <46771423+Piyushvishnoi@users.noreply.github.com>
2020-03-30 01:47:18 +0530

    Update strictnotequalityoperator.js

--- comparison/not equality and strict not equality operator/strictnotequalityoperator.js
+++ comparison/not equality and strict not equality operator/strictnotequalityoperator.js
@@ -3,6 +3,7 @@ function myfun(number) {
     return "not equal";
   } else return "equal";
 }
+//here type conversion will not take place hence string and number cannot be compared 
 console.log(myfun("2"));
 console.log(myfun(2));
 console.log(myfun(3));
