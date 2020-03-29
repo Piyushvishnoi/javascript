ff65087ba4f73bf276228428dde866ae9978275b
Piyush Vishnoi <46771423+Piyushvishnoi@users.noreply.github.com>
2020-03-30 01:31:45 +0530

    Delete comparisionwithequalityoperator.js

--- comparison/comparisionwithequalityoperator.js
+++
@@ -1,8 +0,0 @@
-function myfun(number) {
-  //comparision using equity operator
-  //here string and number can be compared that means type conversion is possible 
-  if (number == 2) {
-    return false;
-  } else return true;
-}
-console.log(myfun(20));
