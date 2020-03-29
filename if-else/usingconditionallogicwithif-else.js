5ffeccf60e92b5f81642903ee866c806cc3688fc
Piyush Vishnoi <46771423+Piyushvishnoi@users.noreply.github.com>
2020-03-29 20:30:29 +0530

    using conditional statement with if-else

---
+++ if-else/usingconditionallogicwithif-else.js
@@ -0,0 +1,10 @@
+function trueorfalse(isThatTrue) {
+  if (isThatTrue) return "Yes that is true";
+  else return "Yes that is false";
+}
+console.log(trueorfalse(true));
+function mytrueorfalse(wasThatFalse) {
+  if (wasThatFalse) return "Yes that was false";
+  else return "Yes that was true";
+}
+console.log(mytrueorfalse(false));
