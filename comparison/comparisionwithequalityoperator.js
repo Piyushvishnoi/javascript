3c4edf3866b7220af80732809ea0df99c306a9df
Piyush Vishnoi <46771423+Piyushvishnoi@users.noreply.github.com>
2020-03-29 23:50:54 +0530

    Update comparisionwithequalityoperator.js

--- comparison/comparisionwithequalityoperator.js
+++ comparison/comparisionwithequalityoperator.js
@@ -1,5 +1,6 @@
 function myfun(number) {
   //comparision using equity operator
+  //here string and number can be compared that means type conversion is possible 
   if (number == 2) {
     return false;
   } else return true;
