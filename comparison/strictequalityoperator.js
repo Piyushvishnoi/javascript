d54fd85e888ca3e97124357a6474df4193cb1058
Piyush Vishnoi <46771423+Piyushvishnoi@users.noreply.github.com>
2020-03-29 23:41:33 +0530

    Update strictequalityoperator.js

--- comparison/strictequalityoperator.js
+++ comparison/strictequalityoperator.js
@@ -1,4 +1,5 @@
 function myfun(number) {
+  //no will be compared to a no then it will be true let if we pass a stringthen we got false
   if (number === 2) {
     return false;
   } else return true;
