83806d924528e26b8441b8ee38b284262cf94a0c
Piyush Vishnoi <46771423+Piyushvishnoi@users.noreply.github.com>
2020-03-29 23:47:58 +0530

    difference

---
+++ comparison/differencebetweenequalityandstrictequalityoperator.js
@@ -0,0 +1,12 @@
+function myfun(a, b) {
+  if (a == b) {
+    return true;
+  } else return false;
+}
+function myfun1(a, b) {
+  if (a === b) {
+    return true;
+  } else return false;
+}
+console.log(myfun(10, "10"));
+console.log(myfun1(10, "10"));
