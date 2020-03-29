0017539d8e0f46f986adc5e9b2a9e8ec6604f307
Piyush Vishnoi <46771423+Piyushvishnoi@users.noreply.github.com>
2020-03-29 23:50:04 +0530

    Update differencebetweenequalityandstrictequalityoperator.js

--- comparison/differencebetweenequalityandstrictequalityoperator.js
+++ comparison/differencebetweenequalityandstrictequalityoperator.js
@@ -1,9 +1,11 @@
 function myfun(a, b) {
+  //in equality operator, we can have type conversion and hence string and number can be compared
   if (a == b) {
     return true;
   } else return false;
 }
 function myfun1(a, b) {
+  //here we don't have type conversion
   if (a === b) {
     return true;
   } else return false;
