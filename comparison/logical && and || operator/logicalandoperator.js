b133f9985b78b4f5598a694ded0b7a1bdfcdf764
Piyush Vishnoi <46771423+Piyushvishnoi@users.noreply.github.com>
2020-03-31 23:48:55 +0530

    Update logicalandoperator.js

--- comparison/logical && and || operator/logicalandoperator.js
+++ comparison/logical && and || operator/logicalandoperator.js
@@ -1,5 +1,5 @@
 function myFun(number) {
-  if (number < 10 && number > 5) console.log("true");
+  if (number < 10 && number > 5) console.log("true");     //in and operator both conditions must be true
   else console.log("true");
 }
 myFun(7);
