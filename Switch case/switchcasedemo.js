38a81edabb6aaebb9fda22300bbc07bc7e04f88d
Piyush Vishnoi <46771423+Piyushvishnoi@users.noreply.github.com>
2020-04-01 23:42:31 +0530

    demo use

---
+++ Switch case/switchcasedemo.js
@@ -0,0 +1,13 @@
+function myfun(number) {
+  switch (number) {
+    case 1:
+      console.log("I am mad");
+      break;
+    case 2:
+      console.log("I am fully mad");
+      break;
+    default:
+      console.log("I am alright");
+  }
+}
+myfun(8);
