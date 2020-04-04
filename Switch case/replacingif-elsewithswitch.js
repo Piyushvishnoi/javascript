fa894867a2fa8ef04264cb87d86106743a110674
Piyush Vishnoi <46771423+Piyushvishnoi@users.noreply.github.com>
2020-04-04 16:08:59 +0530

    replacing if-else chain with switch case

---
+++ Switch case/replacingif-elsewithswitch.js
@@ -0,0 +1,24 @@
+function myFun(number) {
+  if (number == 4) {
+    console.log("Yes it is 4");
+  } else if (number == 38) {
+    console.log("Yes it is 38");
+  } else {
+    console.log("No it is neither 38 nor 4");
+  }
+}
+myFun(38);
+function myFun1(number) {
+  switch (number) {
+    case 4:
+      console.log("Yes it is 4");
+      break;
+    case 38:
+      console.log("Yes it is 38");
+      break;
+    default:
+      console.log("No it is neither 38 nor 4");
+      break;
+  }
+}
+myFun1(3);
