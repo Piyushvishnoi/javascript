5da56cc2db5c32c4fbb83a68946349fcb06eb8e2
Piyush Vishnoi <46771423+Piyushvishnoi@users.noreply.github.com>
2020-04-05 21:51:43 +0530

    returning null or undefined values

---
+++ functions/returningbeforematchingpattern.js
@@ -0,0 +1,13 @@
+function myFun(number) {
+  if (number > 5 && number < 10) {
+    return undefined;
+  } else {
+    if (number == 8) {
+      return "yes number is 8";
+    } else {
+      return "no number is not 8";
+    }
+  }
+}
+
+console.log(myFun(9));
