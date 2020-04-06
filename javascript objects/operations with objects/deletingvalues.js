c1850ef4d4d63a9aee54854930e953076ccb240c
Piyush Vishnoi <46771423+Piyushvishnoi@users.noreply.github.com>
2020-04-06 18:29:05 +0530

    deleting variables in objects

---
+++ javascript objects/operations with objects/deletingvalues.js
@@ -0,0 +1,14 @@
+var myFun = {
+  first: "Piyush",
+  second: "omji",
+  third: "Ameesha",
+  Relation: "Besties",
+};
+myFun.Relation = "BFF";
+//console.log(myFun);
+myFun.occupation = "Studying";
+//console.log(myFun);
+myFun["address"] = "Auraiya";
+console.log(myFun);
+delete myFun.Relation;
+console.log(myFun);
