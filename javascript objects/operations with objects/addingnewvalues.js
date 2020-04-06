9106af28a71fe12917c8c5d47621883a00e2fd30
Piyush Vishnoi <46771423+Piyushvishnoi@users.noreply.github.com>
2020-04-06 18:19:40 +0530

    adding new values to objects

---
+++ javascript objects/operations with objects/addingnewvalues.js
@@ -0,0 +1,12 @@
+var myFun = {
+  first: "Piyush",
+  second: "omji",
+  third: "Ameesha",
+  Relation: "Besties",
+};
+myFun.Relation = "BFF";
+//console.log(myFun);
+myFun.occupation = "Studying";
+//console.log(myFun);
+myFun["address"] = "Auraiya";
+console.log(myFun);
