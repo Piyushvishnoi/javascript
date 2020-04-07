5d5f46ec8cf13ee589cf371030e4e7ad4fbb7111
Piyush Vishnoi <46771423+Piyushvishnoi@users.noreply.github.com>
2020-04-07 21:40:36 +0530

    accessing nested arrays

---
+++ javascript objects/nested arrays/accessingnestedarrays.js
@@ -0,0 +1,12 @@
+var myObj = [
+  {
+    first: "Piyush",
+    second: ["Khushi", "Yash", "Shriyansh"],
+  },
+  {
+    first: "Vishnoi",
+    second: ["Agrawal", "Gupta", "Porwal"],
+  },
+];
+console.log(myObj);
+console.log(myObj[1].second[2]);
