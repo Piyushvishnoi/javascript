66ac4a6fd53c4a53348dd366249cfb9036c874b1
Piyush Vishnoi <46771423+Piyushvishnoi@users.noreply.github.com>
2020-04-07 21:33:59 +0530

    nested arrays in objects

---
+++ javascript objects/nested arrays/nestedarraydemo.js
@@ -0,0 +1,11 @@
+var myObj = [
+  {
+    first: "Piyush",
+    second: ["Khushi", "Yash", "Shriyansh"],
+  },
+  {
+    first: "Vishnoi",
+    second: ["Agrawal", "Gupta", "Porwal"],
+  },
+];
+console.log(myObj);
