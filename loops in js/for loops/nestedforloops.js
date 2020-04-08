d8d7fea7574e58643b0e1bce744f272e7c51ef71
Piyush Vishnoi <46771423+Piyushvishnoi@users.noreply.github.com>
2020-04-08 23:44:59 +0530

    nested for loops

---
+++ loops in js/for loops/nestedforloops.js
@@ -0,0 +1,15 @@
+function multiplyall(arr) {
+  var product = 1;
+  for (var i = 0; i < arr.length; i++) {
+    for (var j = 0; j < arr[i].length; j++) {
+      product *= arr[i][j];
+    }
+  }
+  return product;
+}
+var product = multiplyall([
+  [1, 3],
+  [2, 1],
+  [2, 1, 2],
+]);
+console.log(product);
