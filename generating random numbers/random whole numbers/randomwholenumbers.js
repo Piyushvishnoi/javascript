800be177091dff214cd6da321aad84cbca821717
Piyush Vishnoi <46771423+Piyushvishnoi@users.noreply.github.com>
2020-04-23 00:06:38 +0530

    Update randomwholenumbers.js

--- generating random numbers/random whole numbers/randomwholenumbers.js
+++ generating random numbers/random whole numbers/randomwholenumbers.js
@@ -1,5 +1,5 @@
-var randomNumberBetween0and19 = Math.floor(Math.random() * 20);
+var randomNumberBetween0and19 = Math.floor(Math.random() * 20); //using math.floor function
 function randomWholeNum() {
-  return Math.floor(Math.random() * 10);
+  return Math.floor(Math.random() * 10);  
 }
 console.log(randomWholeNum());
