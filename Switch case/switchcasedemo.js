9808d78d26543e1364a5cb39363517eb16426ea5
Piyush Vishnoi <46771423+Piyushvishnoi@users.noreply.github.com>
2020-04-23 00:10:54 +0530

    Update switchcasedemo.js

--- Switch case/switchcasedemo.js
+++ Switch case/switchcasedemo.js
@@ -3,7 +3,7 @@ function myfun(number) {
     case 1:
       console.log("I am mad");
       break;
-    case 2:
+    case 2: 
       console.log("I am fully mad"); 
       break;
     default:     //means any no other than 1 and 2 will fall under this case i.e default case
