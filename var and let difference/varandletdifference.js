491b688cba054c7be0a4d389ae1722a9185ab51a
Piyush Vishnoi <46771423+Piyushvishnoi@users.noreply.github.com>
2020-04-28 02:49:04 +0530

    Update varandletdifference.js

--- var and let difference/varandletdifference.js
+++ var and let difference/varandletdifference.js
@@ -1,5 +1,5 @@
 var word = "Me";
-var word = "mine";
+var word = "mine";  //var can used for multiple declarations
 console.log(word);
 let word1 = "my let";
 word1 = "my option let"; //let must not be declared more than once
